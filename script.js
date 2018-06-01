ce57d9171dae0d497c21524145fee0eb3a28bc5b
npupiec <nkozlowska89@gmail.com>
2018-06-01 09:20:53 +0200

    additional task caps

--- script.js
+++ script.js
@@ -16,15 +16,11 @@ console.log(`${multiply(a, b)}`)
 
 //task3
 
-let val = 0;
 
-const average = (...args) => {
-    val = args.reduce((x, y) => x + y) / args.length;
-    return val;
-};
+const average = (...args) => args.reduce((x, y) => x + y) / args.length;
+
 
 average(1,2,3,4,5);
-console.log(`${val}`);
 
 //task4
 
@@ -37,3 +33,15 @@ const data = [1, 4, 'Iwona', false, 'Nowak'];
 const [ , , firstname, , lastname] = data;
 
 console.log(firstname, lastname);
+
+//task6 -caps
+
+const names = ["Ania", "Krzyś", "Tomka", "Madzia", "Nina", "Julka", "Mateusz", "Jakuba"];
+const teacherCaps = 237;
+
+function caps() {
+    return names
+        .map(x => 5)
+        .reduce((x,y) => x + y) + teacherCaps;
+}
+console.log(caps())
